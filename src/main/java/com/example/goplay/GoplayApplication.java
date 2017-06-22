e949e24db81c60fe9f705cb889f54c507092645b
PaulKutka <kutka100@gmail.com>
2017-06-22 10:47:09 +0300

    first commit

---
+++ src/main/java/com/example/goplay/GoplayApplication.java
@@ -0,0 +1,12 @@
+package com.example.goplay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoplayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoplayApplication.class, args);
+	}
+}
