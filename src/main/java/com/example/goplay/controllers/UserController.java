d8329bc3e571246b2e8ec5156804e6ccffe65aec
Vilius Zukauskas <Vilius Zukauskas>
2017-06-22 17:30:46 +0300

    getallusers

---
+++ src/main/java/com/example/goplay/controllers/UserController.java
@@ -0,0 +1,24 @@
+package com.example.goplay.controllers;
+
+import com.example.goplay.beans.entity.User;
+import com.example.goplay.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/user")
+public class UserController {
+
+
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping(value = "/all", method = RequestMethod.GET)
+    public Iterable<User> getUsers()
+    {
+        return userService.getAllUsers();
+    }
+
+}
